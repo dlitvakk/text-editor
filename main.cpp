0279144a84d96f85dc3ad3822d4bb0e0271045b0
Dana Litvak <dlitvakk21@gmail.com>
2024-05-26 19:40:02 +0300

    adding new line, loading and saving text

--- main.cpp
+++ main.cpp
@@ -15,11 +15,52 @@ void enter_text(char** current_text) {
     }
 }
 
+void add_new_line(char** current_text) {
+    size_t length_of_current_text = strlen(*current_text);
+    *current_text = (char*)realloc(*current_text, length_of_current_text + 2);
+    strcat(*current_text, "\n");
+}
+
+void save_to_file(char* filename, char* current_text) {
+    FILE *savefile = fopen(filename, "w");
+    if (savefile == nullptr) {
+        printf("Error opening file!\n");
+        return;
+    }
+    fprintf(savefile, "%s", current_text);
+    fclose(savefile);
+
+    printf("Saved to file!\n");
+}
+
+void load_from_file(char* filename, char** current_text) {
+
+    FILE *loadfile = fopen(filename, "r");
+    if (loadfile == nullptr) {
+        printf("Error opening file!\n");
+        return;
+    }
+
+    free(*current_text);
+    *current_text = (char*)malloc(sizeof(char));
+    (*current_text)[0] = '\0';
+
+    char buffer[100];
+    while (fgets(buffer, 100, loadfile) != nullptr) {
+        size_t length_of_current_text = strlen(*current_text);
+        size_t length_of_input = strlen(buffer);
+        *current_text = (char*)realloc(*current_text, length_of_current_text + length_of_input + 1);
+        strcat(*current_text, buffer);
+    }
+    fclose(loadfile);
+
+    printf("Loaded from file!\n");
+}
 
 int main()
 {
 
-    printf("Enter '1' to enter text to the end\n'2' to add new line\n'3' to load TO file\n'4' to load FROM file\n'5' to print the text to console\n'6' to insert text\n'7' to search for text\n");
+    printf("Enter '1' to enter text to the end\n'2' to add new line\n'3' to save TO file\n'4' to load FROM file\n'5' to print the text to console\n'6' to insert text\n'7' to search for text\n '0' to exit\n\n");
     char* string = (char*)malloc(sizeof(char));
     string[0] = '\0';
 
@@ -33,6 +74,34 @@ int main()
                 printf("%s\n", string);
                 break;
             }
+            case '2': {
+                add_new_line(&string);
+                printf("New line created!\n");
+                printf("%s\n", string);
+                break;
+            }
+
+            case '3': {
+                char filename[100];
+                printf("Enter the file name for saving: ");
+                fgets(filename, 100, stdin);
+                filename[strcspn(filename, "\n")] = '\0';
+                // char *filename = "/Users/dlitvakk21/CLionProjects/text-editor/savetext.txt";
+                save_to_file(filename, string);
+                break;
+            }
+
+            case '4': {
+                char filename[100];
+                printf("Enter the file name for saving: ");
+                fgets(filename, 100, stdin);
+                filename[strcspn(filename, "\n")] = '\0';
+                // char *filename = "/Users/dlitvakk21/CLionProjects/text-editor/loadtext.txt";
+                load_from_file(filename, &string);
+                printf("%s\n", string);
+                break;
+            }
+
             case '0': {
                 printf("Thanks for using the program!\n");
                 free(string);
